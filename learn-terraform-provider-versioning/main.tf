9e10784ebdebb355a2cf42c477fe798e8f1b4d1a
Yigit Demirko <ygtdemirko@gmail.com>
2025-07-04 17:09:12 +0300

    feat: Create folder structure to organize all tutorials

---
+++ learn-terraform-provider-versioning/main.tf
@@ -0,0 +1,19 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "random_pet" "petname" {
+  length    = 5
+  separator = "-"
+}
+
+resource "aws_s3_bucket" "sample" {
+  bucket = random_pet.petname.id
+
+  tags = {
+    public_bucket = false
+  }
+}
